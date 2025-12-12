8ff7a3a16c18b4ab6f3f2cf6593efb41492bde99
mdjavee <mdjaveed8201@gmail.com>
2025-12-12 18:19:55 +0530

    new

--- ecommerce/src/components/Hero.jsx
+++ ecommerce/src/components/Hero.jsx
@@ -7,8 +7,8 @@ export const Hero = () => {
         <img src={hero} alt="" className='rounded-md border-4' />
         </div>
         <div className='text-center  p-5  mt-5 '>
-            <h3 className='text-2xl font-extrabold italic '>Welcome to My Website</h3>
-            <p className='font-serif mt-5 m-3 font-medium text-[13px] '>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio veniam repellat cum ipsum amet, porro impedit accusantium iure debitis maiores nulla nesciunt temporibus in possimus soluta, aperiam, quis veritatis illum.</p>
+            <h3 className='text-[1.4rem]  font-extrabold '>Welcome to My Website</h3>
+            <p className='font-sans uppercase mt-5 m-2 font-light text-[13px] '>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio veniam repellat cum ipsum amet, porro impedit accusantium iure debitis maiores nulla nesciunt temporibus in possimus soluta, aperiam, quis veritatis illum.</p>
         </div>
     </div>
   )
